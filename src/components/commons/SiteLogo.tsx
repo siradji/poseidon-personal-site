6d219e6bcc3e69903066ac0f257fc484fb47c473
Suraj Auwal <siradjiawoual@gmail.com>
2022-04-11 02:39:13 +0200

    Adde husky pre commit hook

--- src/components/commons/SiteLogo.tsx
+++ src/components/commons/SiteLogo.tsx
@@ -1,7 +1,7 @@
 export function SiteLogo (): JSX.Element {
   return (
     <div className='text-3xl font-bold flex items-center'>
-      <h1 className='text-white'>Suraj</h1>
+      <h1 className='text-white mr-3'>Suraj</h1>
       <h1 className=' text-secondary-100'>Auwal</h1>
     </div>
   )
